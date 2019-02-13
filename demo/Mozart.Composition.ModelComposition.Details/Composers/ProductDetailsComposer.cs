934cc4082139cb572c2223eeca895246bd0a1760
Scott Thomson <scott.thomson@just-eat.com>
2019-02-13 17:04:04 +0000

    Rename

---
+++ demo/Mozart.Composition.ModelComposition.Details/Composers/ProductDetailsComposer.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Mozart.Composition.Core.Abstractions;
+using Mozart.Composition.ModelComposition.Details.Models;
+
+namespace Mozart.Composition.ModelComposition.Details.Composers
+{
+    public class ProductDetailsComposer : ComposeModel<ProductDetails>
+    {
+        public override async Task<ProductDetails> ComposeOfTAsync(IDictionary<string, object> parameters)
+        {
+            return await Task.FromResult(new ProductDetails
+            {
+                Title = "Sample Product",
+                Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+            });
+        }
+    }
+}
